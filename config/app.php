2fbd2ac4a5b5214e9a26188f6a934fa40c21b89b
Uriah Galang <g0ld3lux@gmail.com>
2017-03-11 16:26:21 +0800

    added laracasts flash method for modals , which required jquery for modal

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         Barryvdh\Cors\ServiceProvider::class,
 	    Laravel\Tinker\TinkerServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Laracasts\Flash\FlashServiceProvider::class,
         
         //
 
