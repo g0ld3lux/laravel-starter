25a489a671ff50bae3626a53cc72675c2a902ddb
Uriah Galang <g0ld3lux@gmail.com>
2017-03-13 06:43:59 +0800

    service for api call

---
+++ resources/assets/js/services/api.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+export default {
+
+  get (url, request) {
+    return axios.get(url, request)
+            .then((response) => Promise.resolve(response.body.data))
+            .catch((error) => Promise.reject(error))
+  },
+  post (url, request) {
+    return axios.post(url, request)
+            .then((response) => Promise.resolve(response))
+            .catch((error) => Promise.reject(error))
+  },
+  patch (url, request) {
+    return axios.patch(url, request)
+            .then((response) => Promise.resolve(response))
+            .catch((error) => Promise.reject(error))
+  },
+  put (url, request) {
+    return axios.put(url, request)
+            .then((response) => Promise.resolve(response))
+            .catch((error) => Promise.reject(error))
+  },
+  delete (url, request) {
+    return axios.delete(url, request)
+            .then((response) => Promise.resolve(response))
+            .catch((error) => Promise.reject(error))
+  },
+  head (url, request) {
+    return axios.head(url, request)
+            .then((response) => Promise.resolve(response))
+            .catch((error) => Promise.reject(error))
+  }
+}
